2ec9ff57c7db3a6ae60e710ea7b345e4825163fe
Ratiesh Dalvi <115468621+codeRatish2@users.noreply.github.com>
2022-11-30 11:53:11 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,15 @@
+var btnTranslate = document.querySelector("btnTraslate");
+var txtInput = document.querySelector("#txtInput");
+var outputDiv = document.querySelector("#output");
+
+
+
+
+
+ 
+function clickHandler(){
+ outputDiv.Innertext = "jssssddjhjas" + txtInput.value;
+};
+
+btnTranslate .addEventListener ("click,",clickHandler)
+
